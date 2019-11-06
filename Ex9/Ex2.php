a935c29883cf8a9bfde2a8c36c9b9da57f9d07ba
Emerson Costa <syphonemerson@yahoo.com.br>
2019-11-05 22:03:25 -0300

    Aula 9

---
+++ Ex9/Ex2.php
@@ -0,0 +1,50 @@
+<?php
+
+$uf = @$_GET['uf'];
+
+if (!isset($uf))
+{
+    $uf = '';
+}
+
+switch ($uf) 
+{
+    case 'SP':
+        $vetor_resposta = 
+            [
+                "Aruja"
+                ,"São Paulo"
+                ,"Princesa Isabel"
+                ,"Cidade 4"
+                ,"Cidade 5"
+            ];  
+        break;
+
+        case 'RJ':
+        $vetor_resposta = 
+            [
+                "Cabo Frio"
+                ,"Rio de Janeiro"
+                ,"Cidade 3"
+                ,"Cidade 4"
+                ,"Cidade 5"
+            ];  
+        break;
+    
+    default:
+        $vetor_resposta = 
+        [
+            "Não encontrado 1"
+            ,"Não encontrado 2"
+            ,"Não encontrado 3"
+            ,"Não encontrado 4"
+            ,"Não encontrado 5"
+        ];    
+
+}
+
+echo json_encode($vetor_resposta);
+return;
+
+?>
+
